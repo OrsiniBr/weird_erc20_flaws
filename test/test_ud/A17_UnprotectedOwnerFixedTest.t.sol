56caf7b9ed58b3cba9e0b9ce63470acb962446a9
OrsiniBr <udochukwugodwin13@gmail.com>
2025-12-31 23:36:31 +0100

    fix the route of import in A17

--- test/test_ud/A17_UnprotectedOwnerFixedTest.t.sol
+++ test/test_ud/A17_UnprotectedOwnerFixedTest.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
-import "../src/UnprotectedOwnerFixed.sol";
+import "../../src/UnprotectedOwnerFixed.sol";
 
 contract UnprotectedOwnerFixedTest is Test {
     UnprotectedOwnerFixed public ownerContract;
